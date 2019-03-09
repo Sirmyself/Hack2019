909669d8e66f786e1b6d4a3e0f8c6ba5f7843a51
WillisCoffee <wthibodeau@hotmail.com>
2019-03-09 10:28:27 -0500

    Ajout du fichier Profile.dart

---
+++ hello_world/lib/Classes/Profile/Profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+
+  @override
+  _ProfileState createState() => new _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      )
+    );
+  }
+}
