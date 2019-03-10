dd1c8d0fcf14bb70295b7f7a63b02e005c1ef049
WillisCoffee <wthibodeau@hotmail.com>
2019-03-10 10:55:29 -0400

    Retrait du "Debug"

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       title: 'HackQC19',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
